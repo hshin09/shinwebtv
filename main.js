ba57fccccef95c9e8d5f6195bfbdf5ccdab66565
hshin09 <56524840+hshin09@users.noreply.github.com>
2019-10-25 20:47:43 +0700

    Add files via upload

--- main.js
+++ main.js
@@ -3,7 +3,8 @@ var tvaddr=new Array(18);
 var addr=[
 ["SBS Golf","http://50.7.118.178:9083/live/lmgr218-live1/dp/Ua/dpUaDQ0LwGNqpgVGdLwsrg==/live.m3u8",null,1],
 ["JTBC Golf","http://23.237.112.138:9083/live/lmgr218-live1/uX/0j/uX0j1KJo8eVhWnTx6uvShw==/live.m3u8",null,1],
-["영화 CGV",null,null,0],
+["영화 CGV",null,null,0]];//,
+/*
 ["영화 OCN","http://50.7.118.178:9083/live/lmgr218-live1/GR/13/GR13XDGjlUsD8nZQasCIhw==/live.m3u8
 ",null,1],
 ["영화 Screen",null,null,0],
@@ -21,7 +22,7 @@ var addr=[
 ["코미디 TV",null,null,0],
 ["XtvN TV",null,null,0]
 ];
-
+*/
 var gi=0;
 var si=7;
 var oi=0;
