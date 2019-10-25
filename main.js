cd29f7f67492e22f352b5c5fec6200e50635567a
hshin09 <56524840+hshin09@users.noreply.github.com>
2019-10-25 18:23:43 +0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,26 @@
 window.onkeydown = keychk;
 var tvaddr=new Array(18);
+var addr=[];
+addr[0]=["SBS GOLF","http://50.7.118.178:9083/live/lmgr218-live1/dp/Ua/dpUaDQ0LwGNqpgVGdLwsrg==/live.m3u8",null,1];
+addr[1]=["JTBC GOLF","http://23.237.112.138:9083/live/lmgr218-live1/uX/0j/uX0j1KJo8eVhWnTx6uvShw==/live.m3u8",null,1];
+addr[2]=["영화 CGV",null,null,0];
+addr[3]=["영화 OCN","http://50.7.118.178:9083/live/lmgr218-live1/GR/13/GR13XDGjlUsD8nZQasCIhw==/live.m3u8
+",null,1];
+addr[4]=["영화 Screen",null,null,0];
+addr[5]=["채널차이나",null,"http://23.237.112.138:9083/live/lmgr218-live1/ut/hX/uthXNC6cyUNTT6dtUu9D6A==/live.m3u8",1];
+addr[6]=["Catch ON 1","http://50.7.118.178:9083/live/lmgr218-live1/Wj/Nz/WjNzluqgVARhAtul5gUKtg==/live.m3u8",null,1];
+addr[7]=["TV 조선","http://23.237.112.138:9083/live/lmgr218-live1/uN/RW/uNRWY94bN9uq-H4U6-AdGA==/live.m3u8",null,1];
+addr[8]=["MBN 뉴스",null,"http://23.237.112.138:9083/live/lmgr218-live1/j7/qy/j7qybKo1-oIO_R2EKcmcIA==/live.m3u8",1];
+addr[9]=["JTBC 뉴스","http://50.7.118.178:9083/live/lmgr218-live1/1k/C9/1kC9miPHbLuH_Xohzycp8g==/live.m3u8",null,1];
+addr[10]=["YTN 뉴스",null,"http://50.7.118.178:9083/live/lmgr218-live1/Y7/kd/Y7kdN2a2F8joMgdxPBzaAQ==/live.m3u8",1];
+addr[11]=["SBS TV","http://50.7.118.178:9083/live/lmgr218-live1/an/Ec/anEch5GmlJzMh-jcs1OTow==/live.m3u8",null,1];
+addr[12]=["MBC TV","http://50.7.118.178:9083/live/lmgr218-live1/yo/Kz/yoKzy5jIzhPSc34OXb26Hg==/live.m3u8",null,1];
+addr[13]=["KBS1 TV","http://23.237.112.138:9083/live/lmgr218-live1/Z1/ck/Z1ckFeyBo9VzK1DFmR1-Hw==/live.m3u8",null,1];
+addr[14]=["KBS2 TV","http://50.7.118.178:9083/live/lmgr218-live1/GJ/qP/GJqPMui6DQrYctmZxQDeig==/live.m3u8",null,1];
+addr[15]=["MNet TV",null,"http://50.7.118.178:9083/live/lmgr218-live1/DD/kh/DDkhewBlWWfGWPVkTkDWNA==/live.m3u8",1];
+addr[16]=["코미디 TV",null,null,0];
+addr[17]=["XtvN TV",null,null,0];
+
 var gi=0;
 var si=7;
 var oi=0;
@@ -22,6 +43,8 @@ $('document').ready(function() {
     $('#menu1').load("https://hshin09.github.io/shinwebtv/thai.html");
     stv = $('#tv').get(0);
     web = document.getElementById("web");
+    for(int i=0; i<tvaddr.length; i++)
+      tvaddr[i]=addr[1];
     timer = setInterval( function() { OnOff(); }, 1100 );
 });
 
@@ -181,9 +204,23 @@ function keychk(e) {
 			onright();
 		}
 		else if(e.which == 13 && gi == 0) {
-		    tvaddr[si] = null;
-		    gettv(si);
-	        x[si].click();
+        var i_ch=addr[si][3];
+        var change_name;
+        if(i_ch!=0) { //보조 또는 대체 채널이 있음
+          if(i_ch==1)  //본채널이었다면 보조채널로
+            i_ch=2;
+          else  //보조채널이었다면 본채널로
+            i_ch=1;
+
+          change_name=x[si].innerHTML;
+          x[si].innerHTML=addr[si][0]
+          addr[si][0]=change_name;
+          tvaddr[si]=addr[si][i_ch];
+          addr[si][3]=i_ch;
+        }
+        if(tvaddr[si] == null)
+		      gettv(si);
+	      x[si].click();
 		}
 }
 
