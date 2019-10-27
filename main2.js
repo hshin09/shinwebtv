a2e7bed4f4e7c1c052a89188c996bceb0efa5279
hshin09 <56524840+hshin09@users.noreply.github.com>
2019-10-27 21:18:33 +0700

    Add files via upload

--- main2.js
+++ main2.js
@@ -110,7 +110,7 @@ function OnOff()
         $("#er_msg").text( "에러 안내 : 채널을 가져올수 없음(네트워크 또는 서버 에러)" );
         showErrorMessage();
         onok();
-    }
+      }
     }
     else if( $('#secMessage').css('display')=="block" && stv.currentTime > 1 )
     {
