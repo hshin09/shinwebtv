431e73e206e7faa0a084f5333f3d7ea36f76e95f
hshin09 <56524840+hshin09@users.noreply.github.com>
2019-10-31 14:42:49 +0700

    Add files via upload

--- main2.js
+++ main2.js
@@ -2,22 +2,22 @@ var tvaddr=new Array(18);
 var addr=[
 ["SBS Golf","SBS Golf","SBS Golf","79","http://50.7.118.178:9083/live/lmgr218-live1/dp/Ua/dpUaDQ0LwGNqpgVGdLwsrg==/live.m3u8",null,3],
 ["JTBC Golf","JTBC Golf","JTBC Golf","79","http://23.237.112.138:9083/live/lmgr218-live1/uX/0j/uX0j1KJo8eVhWnTx6uvShw==/live.m3u8",null,3],
-["영화 CGV","영화 CGV","영화 CGV","79",null,null,3],
+["영화 CGV","영화 CGV","영화 CGV","79",null,"79",3],
 ["영화 OCN","영화 OCN","영화 OCN","79","http://50.7.118.178:9083/live/lmgr218-live1/GR/13/GR13XDGjlUsD8nZQasCIhw==/live.m3u8",null,3],
-["영화 Screen","영화 Screen","영화 Screen","79",null,null,3],
-["Super Action","채널 차이나","Super Action","79","http://23.237.112.138:9083/live/lmgr218-live1/ut/hX/uthXNC6cyUNTT6dtUu9D6A==/live.m3u8",null,3],
+["영화 Screen","영화 Screen","영화 Screen","79",null,"79",3],
+["Super Action","Super Action","채널 차이나","79",null, "http://23.237.112.138:9083/live/lmgr218-live1/ut/hX/uthXNC6cyUNTT6dtUu9D6A==/live.m3u8",3],
 ["Catch ON 1","Catch ON 1","Catch ON 1","79","http://50.7.118.178:9083/live/lmgr218-live1/Wj/Nz/WjNzluqgVARhAtul5gUKtg==/live.m3u8",null,3],
 ["TV 조선 뉴스","TV 조선 뉴스","TV 조선 뉴스","79","http://23.237.112.138:9083/live/lmgr218-live1/uN/RW/uNRWY94bN9uq-H4U6-AdGA==/live.m3u8",null,3],
-["채널A 뉴스","MBN 뉴스","채널A 뉴스","79","http://23.237.112.138:9083/live/lmgr218-live1/j7/qy/j7qybKo1-oIO_R2EKcmcIA==/live.m3u8",null,3],
+["채널A 뉴스","채널A 뉴스","MBN 뉴스","79",null,"http://23.237.112.138:9083/live/lmgr218-live1/j7/qy/j7qybKo1-oIO_R2EKcmcIA==/live.m3u8",3],
 ["JTBC 뉴스","JTBC 뉴스","JTBC 뉴스","79","http://50.7.118.178:9083/live/lmgr218-live1/1k/C9/1kC9miPHbLuH_Xohzycp8g==/live.m3u8",null,3],
-["YTN 뉴스","EBS1","YTN 뉴스","79","http://50.7.118.178:9083/live/lmgr218-live1/Y7/kd/Y7kdN2a2F8joMgdxPBzaAQ==/live.m3u8",null,3],
+["YTN 뉴스","YTN 뉴스","EBS1","79",null,"http://50.7.118.178:9083/live/lmgr218-live1/Y7/kd/Y7kdN2a2F8joMgdxPBzaAQ==/live.m3u8",3],
 ["SBS TV","SBS TV","SBS TV","79","http://50.7.118.178:9083/live/lmgr218-live1/an/Ec/anEch5GmlJzMh-jcs1OTow==/live.m3u8",null,3],
 ["MBC TV","MBC TV","MBC TV","79","http://50.7.118.178:9083/live/lmgr218-live1/yo/Kz/yoKzy5jIzhPSc34OXb26Hg==/live.m3u8",null,3],
 ["KBS1 TV","KBS1 TV","KBS1 TV","79","http://23.237.112.138:9083/live/lmgr218-live1/Z1/ck/Z1ckFeyBo9VzK1DFmR1-Hw==/live.m3u8",null,3],
 ["KBS2 TV","KBS2 TV","KBS2 TV","79","http://50.7.118.178:9083/live/lmgr218-live1/GJ/qP/GJqPMui6DQrYctmZxQDeig==/live.m3u8",null,3],
-["TVN TV","MNet TV","TVN TV","79","http://50.7.118.178:9083/live/lmgr218-live1/DD/kh/DDkhewBlWWfGWPVkTkDWNA==/live.m3u8",null,3],
-["코미디 TV","코미디 TV","코미디 TV","79",null,null,3],
-["XtvN TV","XtvN TV","XtvN TV","79",null,null,3]
+["TVN TV","TVN TV","MNet TV","79",null,"http://50.7.118.178:9083/live/lmgr218-live1/DD/kh/DDkhewBlWWfGWPVkTkDWNA==/live.m3u8",3],
+["코미디 TV","코미디 TV","코미디 TV","79",null,"79",3],
+["XtvN TV","XtvN TV","XtvN TV","79",null,"79",3]
 ];
 
 var gi=0;
