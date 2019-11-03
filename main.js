9806ee6cb73b3fe6aead7298e993889cac0a5af1
hshin09 <56524840+hshin09@users.noreply.github.com>
2019-11-03 16:05:04 +0700

    Add files via upload

--- main.js
+++ main.js
@@ -44,8 +44,8 @@ $('document').ready(function() {
     $('#menu1').load("https://hshin09.github.io/shinwebtv/thai.html");
     stv = $('#tv').get(0);
     web = document.getElementById("web");
-    web.setAttribute( "target", "about:blank");
-    web.setAttribute( "src", "https://www.adintrend.tv/hd/m/ch35" );
+    //web.setAttribute( "target", "about:blank");
+    //web.setAttribute( "src", "https://www.adintrend.tv/hd/m/ch35" );
 
     for(var i=0; i<tvaddr.length; i++)
       tvaddr[i]=addr[i][3];
