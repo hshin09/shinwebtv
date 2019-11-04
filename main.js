b7ef4a6775a7920f14550e7b2bf5bc21a3ff4bb4
hshin09 <56524840+hshin09@users.noreply.github.com>
2019-11-04 11:13:30 +0700

    Add files via upload

--- main.js
+++ main.js
@@ -372,7 +372,7 @@ function movieclk( w, url, p ) {
       if(url.length>10) {
         var s = url;
         var s1 = s.substring(0,10);
-        if( s1 == "adintrend:" )
+        if( s1 == "adintrend:" ) {
           if( getADintrend(s.substring(10)) ) {
             url = demostr;
             alert(url);
