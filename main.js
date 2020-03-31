bdfd2081d008b4a71ec77ac44b428f0598a8d80a
hshin09 <56524840+hshin09@users.noreply.github.com>
2020-03-31 11:09:39 +0700

    Update main.js

--- main.js
+++ main.js
@@ -281,13 +281,10 @@ function keychk(e) {
 		}
 	    else if(e.which == 48 ) {
 	      $('#menu1').load("https://hshin09.github.io/shinwebtv/thai.html");
-              si = ei = 12;
 	    }
 	    else if(e.which == 49 ) {
 	      if( ei==7 && si==9 ) {
 		$('#menu1').load("https://hshin09.github.io/shinwebtv/svideo.html");
-                vsi = ei;
-                si = ei = 0;
 	      }
 	    }
 	    else if(e.which == 53 ) {
