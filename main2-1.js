465f5bcb2b3f6e591eb41147f502aa37425acfc8
hshin09 <56524840+hshin09@users.noreply.github.com>
2020-04-04 10:57:48 +0700

    Update main2-1.js

--- main2-1.js
+++ main2-1.js
@@ -70,7 +70,7 @@ function addInput()
    input.style.display = "block";
    input.style.top = "50px";
    input.style.left = "50px";
-   input.onkeypress="javascript:if(event.keyCode == 13){alret('abc');}";
+   input.onkeypress="javascript:if(event.keyCode == 13){alert('abc');}";
    document.body.appendChild( input );
 }
 
