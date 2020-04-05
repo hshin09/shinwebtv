57e49a0b1e01eb86052ae31a5ea2e82569d07acc
hshin09 <56524840+hshin09@users.noreply.github.com>
2020-04-05 10:02:10 +0700

    Update main2-1.js

--- main2-1.js
+++ main2-1.js
@@ -70,7 +70,7 @@ function addInput()
    input.style.display = "block";
    input.style.top = "50px";
    input.style.left = "50px";
-   input.onkeydown="function(){if(event.which == 13){alert('abc');}}";
+   input.onKeyDown="javascript:function(){if(event.which == 13){alert('abc');}}";
    document.body.appendChild( input );
 }
 
