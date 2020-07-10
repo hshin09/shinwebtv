a4ae3a62ca338f2b32f16e2d2ed06c4a3ceae73f
hshin09 <56524840+hshin09@users.noreply.github.com>
2020-07-10 16:31:00 +0700

    Update main2-2.js

--- main2-2.js
+++ main2-2.js
@@ -55,6 +55,7 @@ $('document').ready(function() {
     $('#menu1').load("https://hshin09.github.io/shinwebtv/thai.html");
     stv = $('#tv').get(0);
     web = document.getElementById("web");
+    web.setAttribute( "src", "http://youtv24.com/sites/" );
     for(var i=0; i<tvaddr.length; i++)
       tvaddr[i]=addr[i][3];
     /*
