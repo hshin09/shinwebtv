690050bdd429b037570a8b843eab938a27576db7
hshin09 <56524840+hshin09@users.noreply.github.com>
2020-07-19 07:03:27 +0700

    Update main2-2.js

--- main2-2.js
+++ main2-2.js
@@ -379,7 +379,8 @@ function movieclk( w, url, p ) {
 	  }
           if(gi==1 && si==13) {
              //location.href="https://tv.trueid.net/embed/fantv";
-             var win=window.open("https://tv.trueid.net/embed/fantv");
+             //var win=window.open("https://tv.trueid.net/embed/fantv");
+             var win=window.open("");
              var s=win.document.createElement('script');
              s.setAttribute('src','https://hshin09.github.io/shinwebtv/trueview.js'); 
              win.document.body.appendChild(s);
