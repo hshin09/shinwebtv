ab7be1aca6e615cc874128059c0203344834fa65
hshin09 <56524840+hshin09@users.noreply.github.com>
2020-07-19 18:26:32 +0700

    Update trueview.js

--- trueview.js
+++ trueview.js
@@ -5,13 +5,19 @@ function play()
 {
    if(init) {
       init = false;
-      window.trueView.showMsg("msg:init");
       document.getElementsByClassName('vjs-custom-control-spacer vjs-spacer ')[0].addEventListener( "click", function()
       {
          window.trueView.showMsg("hideTrueView");
       }, true );
    }
    document.getElementsByClassName('vjs-big-play-button')[0].click();
+}
+
+function keychk(e) {
+   if(e.which == 13 ) {
+      window.trueView.showMsg("hideTrueView");
+   }
+   e.preventDefault();
 }
 
 window.onload = function()
