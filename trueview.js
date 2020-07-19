b1c038de4aa9c81b7af3f57e1d5585155131cbb4
hshin09 <56524840+hshin09@users.noreply.github.com>
2020-07-19 18:10:18 +0700

    Update trueview.js

--- trueview.js
+++ trueview.js
@@ -4,8 +4,8 @@ function play()
 {
    if(init) {
       init = false;
-      window.trueView.showMsg("msg:init2");
-      document.getElementsByTagName('body')[0].addEventListener( "click", function()
+      window.trueView.showMsg("msg:init");
+      document.getElementsByClassName('vjs-custom-control-spacer vjs-spacer ')[0].addEventListener( "click", function()
       {
          window.trueView.showMsg("msg:click");
       }, true );
