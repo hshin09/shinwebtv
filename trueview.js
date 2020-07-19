7cfba559499d68174c0f6657aa4a07842ce1ea55
hshin09 <56524840+hshin09@users.noreply.github.com>
2020-07-19 16:48:46 +0700

    Update trueview.js

--- trueview.js
+++ trueview.js
@@ -5,8 +5,11 @@ function play()
    document.getElementsByClassName('vjs-big-play-button')[0].click();
 }
 
-document.getElementsByClassName('vjs-tech')[0].addEventListener( "click", function()
+document.addEventListener( "DOMContentLoaded", function()
 {
-   alert('click');
+   document.getElementsByClassName('vjs-tech')[0].addEventListener( "click", function()
+   {
+      alert('click');
+   } );
 } );
 
