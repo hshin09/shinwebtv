36f192a3e9e63cd3954ef8b73b204cfd24df3d01
hshin09 <56524840+hshin09@users.noreply.github.com>
2020-07-19 14:48:35 +0700

    Update trueview.js

--- trueview.js
+++ trueview.js
@@ -1,12 +1,10 @@
 function play()
 {
-   window.trueView.showMsg("msg:play");
    document.getElementsByClassName('vjs-big-play-button')[0].click();
 }
 
-window.onload = function(){
-   play();
-   window.trueView.showMsg("showTrueView");
+window.onload = function()
+{
 }
 
 
