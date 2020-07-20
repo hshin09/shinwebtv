7f0dc45705318fe4d3ae2ed9370a301c3c65510f
hshin09 <56524840+hshin09@users.noreply.github.com>
2020-07-20 11:54:56 +0700

    Update main2-2.js

--- main2-2.js
+++ main2-2.js
@@ -378,7 +378,7 @@ function movieclk( w, url, p ) {
       xx=stv;
 	  }
           if(gi==1 && si==13) {
-             window.parentView.showMsg("trueViewLoadUrl:https://tv.trueid.net/embed/fantv");
+             setTimeout(function(){ window.parentView.showMsg("trueViewLoadUrl:https://tv.trueid.net/embed/fantv"); },1000);
              //window.parentView.showMsg("trueView:play()");
              //window.parentView.showMsg("showTrueView");
              return;
