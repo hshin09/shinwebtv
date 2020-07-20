4bcde91c22daff98c7dfe57d0e5e69153528ed68
hshin09 <56524840+hshin09@users.noreply.github.com>
2020-07-20 18:34:18 +0700

    Update trueview.js

--- trueview.js
+++ trueview.js
@@ -19,23 +19,24 @@ function play()
          window.trueView.showMsg("hideTrueView");
       }, true );
    }
-   document.getElementsByClassName('vjs-poster')[0].click();
-   setTimeout(function(){ document.getElementsByClassName('vjs-big-play-button')[0].click(); },500 );
-   setTimeout(function(){ document.getElementsByClassName('vjs-live-status vjs-live-status-live')[0].click(); },500 );
+   setTimeout(function(){ document.getElementsByClassName('vjs-poster')[0].click(); }, 300 );
+   setTimeout(function(){ document.getElementsByClassName('vjs-big-play-button')[0].click(); },300 );
+   setTimeout(function(){ document.getElementsByClassName('vjs-live-status vjs-live-status-live')[0].click(); },300 );
 }
 
 function keychk(e) {
    if(e.which == 38 || e.which == 48 ) {
       window.trueView.showMsg("hideTrueView");
    } else if( e.which == 13) {
-      document.getElementsByClassName('vjs-big-play-button')[0].click();
-      document.getElementsByClassName('vjs-poster')[0].click();
+      setTimeout(function(){ document.getElementsByClassName('vjs-poster')[0].click(); }, 300 );
+      setTimeout(function(){ document.getElementsByClassName('vjs-big-play-button')[0].click(); },300 );
+      setTimeout(function(){ document.getElementsByClassName('vjs-live-status vjs-live-status-live')[0].click(); },300 );
    }
    e.preventDefault();
 }
 
 window.onload = function()
 {
-   play();
+   setTimeout(function(){ play(); }, 5000);
 }
 
