c8139f4d8e0a993aec2b98751f1eef79a1faf272
hshin09 <56524840+hshin09@users.noreply.github.com>
2020-07-26 13:30:33 +0700

    Update main2-2.js

--- main2-2.js
+++ main2-2.js
@@ -149,7 +149,7 @@ function OnOff()
           {
              window.parentView.showMsg( "hiddenView:loadTV('" + path + ch[ei] + "')" );
              //window.parentView.showMsg( "msg:채널을 리로딩중입니다:"+isNotUser);
-             mustWait = 2;
+             mustWait = 1;
              mustabout = 1;
              timeSetTV = 500;
              clearAddress(addr[ei][addr[ei][6]]);
