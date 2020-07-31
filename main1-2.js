96bcaac703ba94039b2471c532878c66742107d5
hshin09 <56524840+hshin09@users.noreply.github.com>
2020-07-31 09:41:31 +0700

    Update main1-2.js

--- main1-2.js
+++ main1-2.js
@@ -60,7 +60,7 @@ $('document').ready(function() {
     $('#menu0').load("https://hshin09.github.io/shinwebtv/kor2.html");
     $('#menu1').load("https://hshin09.github.io/shinwebtv/thai.html");
     stv = $('#tv').get(0);
-    stv.onwaiting = function(){ window.parentView.showMsg( "msg:wating" ); }
+    //stv.onwaiting = function(){ window.parentView.showMsg( "msg:wating" ); }
     stv.onabort = function(){ window.parentView.showMsg( "msg:abort" ); }
     web = document.getElementById("web");
     for(var i=0; i<tvaddr.length; i++)
