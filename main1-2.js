ad13ad86298cd2d20464dbf911e4e9fa66e3d169
hshin09 <56524840+hshin09@users.noreply.github.com>
2020-07-31 09:30:26 +0700

    Update main1-2.js

--- main1-2.js
+++ main1-2.js
@@ -61,7 +61,7 @@ $('document').ready(function() {
     $('#menu1').load("https://hshin09.github.io/shinwebtv/thai.html");
     stv = $('#tv').get(0);
     stv.onwaiting = function(){ onok(); }
-    stv.onabort = function(){ onok(); }
+    //stv.onabort = function(){ onok(); }
     web = document.getElementById("web");
     for(var i=0; i<tvaddr.length; i++)
       tvaddr[i]=addr[i][3];
