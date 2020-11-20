0cb7c02f48db99008ad73d1ab1629b96482688df
hshin09 <56524840+hshin09@users.noreply.github.com>
2020-11-20 13:15:50 +0700

    Update main1-2.js

--- main1-2.js
+++ main1-2.js
@@ -143,7 +143,7 @@ function OnOff()
           {
              //window.parentView.showMsg( "hiddenView:loadTV('" + path + ch[ei] + "')" );
              //window.parentView.showMsg( "msg:채널을 리로딩중입니다:"+isNotUser);
-             mustWait = 5;
+             mustWait = 7;
              mustabout = 1;
              timeSetTV = 800;
              clearAddress(addr[ei][addr[ei][6]]);
@@ -243,9 +243,8 @@ function showTime()
 
 function get79tv(i) 
 {
-   if( isNotUser < 1 )
-      window.parentView.showMsg( "hiddenView:loadTV('" + path + ch[i] + "')" );
-   mustWait = 5;
+   window.parentView.showMsg( "hiddenView:loadTV('" + path + ch[i] + "')" );
+   mustWait = 9;
    //window.parentView.showMsg("79:"+path79+ch[i]);
    window.parentView.showMsg("hiddenView:gettv('"+path79+ch[i]+"')");
 }
