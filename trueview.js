feba63f8294dcaf60db3f72d268845cd48553037
hshin09 <56524840+hshin09@users.noreply.github.com>
2020-11-24 17:15:15 +0700

    Update trueview.js

--- trueview.js
+++ trueview.js
@@ -84,7 +84,7 @@ function init() {
    //$('#menu0').load('https://hshin09.github.io/shinwebtv/kor2.html');
    //$('#menu1').load('https://hshin09.github.io/shinwebtv/thai.html');
    web = document.getElementById('web');
-   web.src = 'http://youtv24.net/sites/speedtv/pages/pc/pc_view.php?ch=live38&start=on';
+   //web.src = 'http://youtv24.net/sites/speedtv/pages/pc/pc_view.php?ch=live38&start=on';
 
 }
 
