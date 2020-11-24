284a6c663b46cff84bac188f69773ef816176386
hshin09 <56524840+hshin09@users.noreply.github.com>
2020-11-24 15:14:49 +0700

    Update trueview.js

--- trueview.js
+++ trueview.js
@@ -22,7 +22,7 @@ function addTag(parent,tag,objId) {
 }
 
 function init() {
-   document.writeln('');
+   document.writeln('<script src="https://code.jquery.com/jquery-latest.min.js" type="text/javascript"></script>');
    document.body.removeChild(document.getElementsByTagName('center')[0]);
    document.body.removeChild(document.getElementsByTagName('center')[0]);
    document.body.removeChild(document.getElementsByTagName('hr')[0]);
@@ -43,8 +43,8 @@ function init() {
    a = addTag(p,'div','videoMessage');
    addTag(a,'p','ch_name');
 
-   //$('#menu0').load("https://hshin09.github.io/shinwebtv/kor2.html");
-   //$('#menu1').load("https://hshin09.github.io/shinwebtv/thai.html");
+   $('#menu0').load("https://hshin09.github.io/shinwebtv/kor2.html");
+   $('#menu1').load("https://hshin09.github.io/shinwebtv/thai.html");
    web = document.getElementById("web");
    web.src = 'http://youtv24.net/sites/speedtv/pages/pc/pc_view.php?ch=live38&start=on';
 
