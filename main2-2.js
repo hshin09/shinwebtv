58a3af69df3f8b915d89fd2393c11834437ab3db
hshin09 <56524840+hshin09@users.noreply.github.com>
2020-11-25 04:31:21 +0700

    Update main2-2.js

--- main2-2.js
+++ main2-2.js
@@ -52,7 +52,8 @@ var msgGetCh="채널리스트 구성중";
 var oldCurrentTime=0;
 
 $('document').ready(function() {
-    window.parentView.showMsg("msg:main2");
+    //window.parentView.showMsg("msg:main2");
+    alert('main');
     $('#menu0').load("https://hshin09.github.io/shinwebtv/kor2.html");
     $('#menu1').load("https://hshin09.github.io/shinwebtv/thai.html");
     stv = $('#tv').get(0);
