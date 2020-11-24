4218284b3659220456958b6ecb0057877f24675a
hshin09 <56524840+hshin09@users.noreply.github.com>
2020-11-24 13:33:51 +0700

    Update trueview.js

--- trueview.js
+++ trueview.js
@@ -1,24 +1,33 @@
+
 window.onload = function()
 {
    document.getElementsByTagName('center')[0].remove();
    document.getElementsByTagName('center')[0].remove();
    document.getElementsByTagName('hr')[0].remove();
 
-   var a = addTag(null,'iframe','player');
-   a.setAttribute('frameborder', '0');
-   a.setAttribute('border', '0');
-   a.setAttribute('width', '100%');
-   a.setAttribute('height', '100%');
+   var p = addTag(null,'div','left');
+   var a = addTag(p,'div','menu0');
+   a = addTag(p,'div','menu1');
+
+   p = addTag(null,'div','myid');
+   a = addTag(p,'iframe','web');
+   a.setAttribute('frameborder','0');
+   a.setAttribute('border','0');
+   a.setAttribute('width','100%');
+   a.setAttribute('height','100%');
+   a = addTag(p,'div','videoMessage');
+   addTag(a.'p','ch_name');
 }
 
 function addTag(parent,tag,objId) {
-	var iTag = document.createElement('tag');
-	iTag.setAttribute('id', objId);
-        if(parent)
-           parent.appendChild(iTag);
-        else
-	   document.body.appendChild(iTag);
-        return iTag;
+   var iTag = document.createElement('tag');
+   iTag.setAttribute('id', objId);
+   if(parent)
+      parent.appendChild(iTag);
+   else
+      document.body.appendChild(iTag);
+   
+   return iTag;
 }
 
 
