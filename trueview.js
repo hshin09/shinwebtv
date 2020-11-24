55bde6325f67b4ca553953d7af8ecf32892fd559
hshin09 <56524840+hshin09@users.noreply.github.com>
2020-11-24 15:10:21 +0700

    Update trueview.js

--- trueview.js
+++ trueview.js
@@ -1,4 +1,4 @@
-//var web;
+var web;
 
 function addFrame(objId) {
 	var iFrm = document.createElement('iframe');
@@ -9,7 +9,7 @@ function addFrame(objId) {
 	iFrm.setAttribute('height', '100%');
 	document.body.appendChild(iFrm);
 }
-/*
+
 function addTag(parent,tag,objId) {
    var iTag = document.createElement(tag);
    iTag.setAttribute('id', objId);
@@ -20,16 +20,16 @@ function addTag(parent,tag,objId) {
    
    return iTag;
 }
-*/
+
 function init() {
+   document.writeln('');
    document.body.removeChild(document.getElementsByTagName('center')[0]);
    document.body.removeChild(document.getElementsByTagName('center')[0]);
    document.body.removeChild(document.getElementsByTagName('hr')[0]);
 
-   addFrame('player');
-   document.getElementById('player').src='http://youtv24.net/sites/speedtv/pages/pc/pc_view.php?ch=live38&start=on';
+//   addFrame('player');
+//   document.getElementById('player').src='http://youtv24.net/sites/speedtv/pages/pc/pc_view.php?ch=live38&start=on';
 
-/*
    var p = addTag('','div','left');
    var a = addTag(p,'div','menu0');
    a = addTag(p,'div','menu1');
@@ -47,7 +47,7 @@ function init() {
    $('#menu1').load("https://hshin09.github.io/shinwebtv/thai.html");
    web = document.getElementById("web");
    web.src = "http://youtv24.net/sites/speedtv/pages/pc/pc_view.php?ch=live38&start=on";
-*/
+
 }
 
 init();
