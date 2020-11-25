5066fdd500f0da377ad0fc814059f0d2f232770d
hshin09 <56524840+hshin09@users.noreply.github.com>
2020-11-25 10:53:42 +0700

    Update trueview.js

--- trueview.js
+++ trueview.js
@@ -471,7 +471,18 @@ function loadMenu(id,url) {
    xhr.onreadystatechange= function() {
       if (this.readyState!==4) return;
       if (this.status!==200) return; // or whatever error handling you want
-      document.getElementById(id).innerHTML= this.responseText;
+      document.getElementById(id).innerHTML = this.responseText;
+   };
+   xhr.send();
+}
+
+function loadVideo(id,url) {
+   var xhr= new XMLHttpRequest();
+   xhr.open('GET', url, true);
+   xhr.onreadystatechange= function() {
+      if (this.readyState!==4) return;
+      if (this.status!==200) return; // or whatever error handling you want
+      document.getElementById(id).src = this.responseText;
    };
    xhr.send();
 }
