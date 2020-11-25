b3e7a06dece20f8b3f06cdaddbea20b43c516941
hshin09 <56524840+hshin09@users.noreply.github.com>
2020-11-25 08:56:32 +0700

    Update trueview.js

--- trueview.js
+++ trueview.js
@@ -491,7 +491,7 @@ function init() {
 
    p = addTag('','div','mydiv');
    a = addTag(p,'iframe','web');
-   //a.setAttribute('allowFullscreen','true');
+   a.setAttribute('allowFullscreen','true');
    a.setAttribute('frameborder','0');
    a.setAttribute('border','0');
    a.setAttribute('width','100%');
