49eda8e26a7a05afd46ba52571affcfb22e2d186
hshin09 <56524840+hshin09@users.noreply.github.com>
2020-11-25 15:05:31 +0700

    Update trueview.js

--- trueview.js
+++ trueview.js
@@ -497,7 +497,7 @@ function loadMenu(id,url) {
 function loadVideo(url) {
    web.src = url;
    //strResponse = web.contentWindow.document.body.innerHTML;
-   strResponse = web.contentDocument.getElementsByTagName('script').innerHTML;
+   strResponse = web.contentDocument.getElementsByTagName('script')[0].innerHTML;
    alert( strResponse );
    var ssi = strResponse.indexOf("file: \"http");
    if(ssi<1) {
