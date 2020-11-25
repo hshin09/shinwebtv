d348325261243306e7d80a500c6883723e291a28
hshin09 <56524840+hshin09@users.noreply.github.com>
2020-11-25 08:16:38 +0700

    Update trueview.js

--- trueview.js
+++ trueview.js
@@ -475,7 +475,7 @@ function loadMenu(id,url) {
 function init() {
    var meta=document.createElement('meta');
    meta.name='viewport';
-   meta.setAttribute('content', 'width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0');
+   meta.setAttribute('content', 'width=device-width, height=device-height, initial-scale=1.0');
    document.getElementsByTagName('head')[0].appendChild(meta);
 
    loadStyle('https://hshin09.github.io/shinwebtv/main.css');
