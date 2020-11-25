8e965bada33b4243c626298bb499016bdb190cd8
hshin09 <56524840+hshin09@users.noreply.github.com>
2020-11-25 13:44:58 +0700

    Update trueview.js

--- trueview.js
+++ trueview.js
@@ -500,7 +500,7 @@ function loadVideo(url) {
    xhr.onreadystatechange= function() {
       if (this.readyState!==4) return;
       if (this.status!==200) return; // or whatever error handling you want
-      web.src = this.responseText;
+      web.innerHTML = this.responseText;
 
       strResponse = this.responseText;
       var ssi = strResponse.indexOf("file: \"http");
