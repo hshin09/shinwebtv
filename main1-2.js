bcf221f322fe111832ad252392ea5b317fe07cc0
hshin09 <56524840+hshin09@users.noreply.github.com>
2020-11-26 10:45:21 +0700

    Update main1-2.js

--- main1-2.js
+++ main1-2.js
@@ -543,7 +543,9 @@ function movieclk( w, url, p ) {
        url = pathmyshtv + url + "playlist.m3u8";
 
     stv.volume=1;
-    if( gi==1 && p.id > 18 )
+    if( svideo == 1 && gi==1 )
+       stv.volume=0.3;
+    else if( svideo == 0 && gi==1 && p.id > 18 )
     {
       stv.volume=0.2;
       if( p.id == 19 || p.id == 21 )
@@ -553,7 +555,7 @@ function movieclk( w, url, p ) {
       else if( p.id == 23 )
          stv.volume=0.3;
     }
-    else if( gi == 1 ) {
+    else if( svideo == 0 && gi == 1 ) {
        if( url.indexOf("p1.cdn.vet") > 0 ) {
           stv.volume=0.3;
           url = url + ADsid;
