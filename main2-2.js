a82d67f9ad97f556c68bebe621273a05ac1644b5
hshin09 <56524840+hshin09@users.noreply.github.com>
2020-11-27 13:06:38 +0700

    Update main2-2.js

--- main2-2.js
+++ main2-2.js
@@ -174,7 +174,7 @@ function OnOff()
           $("#er_msg").text( "채널을 가져올수 없음(네트워크 또는 서버 에러)-Timer" );
           showErrorMessage();
        }
-       if(isNotUser<0) {
+       if(isNotUser<1) {
           if( gi == 0 && myshtv == 0 )
           {
              //window.parentView.showMsg( "hiddenView:loadTV('" + path + ch[ei] + "&start=on')" );
@@ -457,7 +457,8 @@ function onok() {
     if(i_ch>4)
       i_ch=3;
     
-    //tvaddr[si]=addr[si][i_ch];
+    if( addr[si][i_ch] != '79' || tvaddr[si] != '79' )
+       tvaddr[si] = addr[si][i_ch];
 
     x[si].innerHTML=addr[si][i_ch-3];
 
