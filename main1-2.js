8efa8242d59430dc44416ce5869035aca7d25d79
hshin09 <56524840+hshin09@users.noreply.github.com>
2020-11-28 08:26:20 +0700

    Update main1-2.js

--- main1-2.js
+++ main1-2.js
@@ -5,12 +5,14 @@ $('document').ready(function() {
    var scr  = document.createElement('script');
    scr.src = "https://hshin09.github.io/shinwebtv/common.js";
    head.appendChild(scr);
+   /*
    alert('s');
    window.onload = function() {
       alert('o');
       Init();
    }
    alert('e');
+   */
    //touchscreen = 0;
 });
 
