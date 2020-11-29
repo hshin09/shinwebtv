ec3a199aaf3ea11e8d471ed634b3bf97679bcce3
hshin09 <56524840+hshin09@users.noreply.github.com>
2020-11-29 07:05:31 +0700

    Update common.js

--- common.js
+++ common.js
@@ -148,14 +148,14 @@ function OnOff()
           $("#er_msg").text( "채널을 가져올수 없음(네트워크 또는 서버 에러)-Timer" );
           showErrorMessage();
        }
-       if(isNotUser<1) {
+       if(isNotUser<0) {
           if( gi == 0 && myshtv == 0 )
           {
              //window.parentView.showMsg( "hiddenView:loadTV('" + path + ch[ei] + "&start=on')" );
              //mustWait = 5;
              mustabout = 1;
              timeSetTV = 500;
-             clearAddress(addr[ei][addr[ei][6]]);
+             //clearAddress(addr[ei][addr[ei][6]]);
           }
           isNotUser++;
        }
@@ -169,8 +169,10 @@ function OnOff()
           isNotUser=0;
           if( gi == 0 ) {
              var ich = addr[ei][6];
-             if( addr[ei][ich] == '79' )
+             if( addr[ei][ich] == '79' ) {
                 addr[ei][6] = ich-1;
+                clearAddress(addr[ei][addr[ei][6]]);
+             }
              setTimeout(function(){onok();},timeSetTV);
           }
        }
