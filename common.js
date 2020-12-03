3c7c5b26105323f488c9a3a39a94d668e0f566bb
hshin09 <56524840+hshin09@users.noreply.github.com>
2020-12-03 07:15:52 +0700

    Update common.js

--- common.js
+++ common.js
@@ -102,7 +102,7 @@ function loadMyShTV(url) {
       str = str.substring(ssi,eei);
       pathmyshtv = str;
       isLoadedMyshtv = 1;
-      window.parentView.showMsg("msg:"+pathmyshtv + " ts=" + touchscreen);
+      window.parentView.showMsg("msg:" + pathmyshtv);
    }; 
    xhr.send();
 }
