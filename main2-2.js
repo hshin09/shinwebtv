ff30771cf5f5a3f5db6d7251f66b887c01182d8b
hshin09 <56524840+hshin09@users.noreply.github.com>
2020-12-03 13:45:20 +0700

    Update main2-2.js

--- main2-2.js
+++ main2-2.js
@@ -71,59 +71,12 @@ function firstSetting()
    addEventListener('touchend', handleTouchEnd, false);
    $('#tv').on('dblclick',(function(){ onFullscreenOnOff(); }));
    $('#tv').on('click',(function(){ onFullscreenOnOff(); }));
-   addInput();
 }
 
 function onFinish() {
-   //if( gi == 1 && svideo == 0 && si == 10 ) {
-   if( si == 22 ) {
-      $('#pwd').css('display','block');
-      return;
-   }
    if(timer)
       clearInterval(timer);
    stv.pause();
    stv.setAttribute( "src",  "" );
    parentView.showMsg("finish");
-}
-
-function addInput()
-{
-   var input = document.createElement('input');
-   input.type = "password";
-   input.id = "pwd";
-   input.value = "";
-   input.style.position ="absolute";
-   input.style.display = "none";
-   input.style.top = "50px";
-   input.style.left = "50px";
-   document.body.appendChild( input );
-   $("input").keydown( function(e) { 
-      $('#pwd').css('display','none');
-      if( e.which == 55 || e.keyCode == 55 ) { //7
-         if( gi == 0 ) {
-            stv.pause();
-            if(timer) {
-               clearInterval(timer);
-               timer=null;
-            } 
-            window.parentView.showMsg("trueView:loadMode = 0");
-            window.parentView.showMsg("showTrueView");
-            window.parentView.showMsg("trueView:x[si].click()");
-         } else {
-            if( svideo == 0 ) {
-               svideo = 1;
-               $('#menu1').load("https://hshin09.github.io/shinwebtv/svideo.html");
-            } else {
-               svideo = 0;
-               $('#menu1').load("https://hshin09.github.io/shinwebtv/thai.html");
-            }
-            setTimeout(function(){ mlok(); }, 700);
-         }
-      }
-      else {
-         si=0; 
-         onFinish();
-      } 
-   });
 }
