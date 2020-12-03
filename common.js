604f69d45fc60be8a60d341fb1b32557782fc675
hshin09 <56524840+hshin09@users.noreply.github.com>
2020-12-03 14:09:53 +0700

    Update common.js

--- common.js
+++ common.js
@@ -164,7 +164,7 @@ function OnOff()
        return;
     }
 
-    if( stv.error != null || stv.networkState == 3 || ( time > 30 && stv.currentTime < 2 ) )
+    if( stv.error != null || stv.networkState == 3 || ( time > 15 && stv.currentTime < 2 ) )
     {
        if( stv.src.substring(0,4) == "file" )
           return;
