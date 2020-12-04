812ef0bf1d4b1d9c2c0c2c6b5f2ca0d3c494ab22
hshin09 <56524840+hshin09@users.noreply.github.com>
2020-12-04 09:34:58 +0700

    Update trueview.js

--- trueview.js
+++ trueview.js
@@ -492,7 +492,7 @@ function init() {
    ch_name = document.getElementById('ch_name');
    tv.style.display = "none";
    web.onload = function() {
-      web.style.height = window.screen.height;;
+      web.style.height = window.screen.height + 20;
       alert(web.style.height);
    };
 }
