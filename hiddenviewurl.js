02b93a6862fb786771480db319a68d541aa01aec
hshin09 <56524840+hshin09@users.noreply.github.com>
2020-12-06 13:02:21 +0700

    Update hiddenviewurl.js

--- hiddenviewurl.js
+++ hiddenviewurl.js
@@ -8,5 +8,5 @@ function addFrame(objId)
 }
 addFrame('TV');
 document.getElementById('TV').src='https://www.adintrend.tv/hd/live/i.php?ch=3&cxid=sh';
-window.hiddenView.showMsg( "webView:ADsid = 'sh';" );
+window.hiddenView.showMsg( "webView:var ADsid = 'sh';" );
 
