687c8f2e510fd223817670a09ec893a577ff88d4
hshin09 <56524840+hshin09@users.noreply.github.com>
2020-12-06 11:03:48 +0700

    Update trueview.js

--- trueview.js
+++ trueview.js
@@ -440,8 +440,6 @@ function init() {
       loadStyle('https://hshin09.github.io/shinwebtv/main2.css'); 
       addEventListener('touchstart', handleTouchStart, false);
       addEventListener('touchend', handleTouchEnd, false);
-      web.addEventListener('touchstart', handleTouchStart, false);
-      web.addEventListener('touchend', handleTouchEnd, false);
    }
    else {
       loadStyle('https://hshin09.github.io/shinwebtv/youtvkor.css');
