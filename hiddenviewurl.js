37b0da7ac246b4f694ef74c4738272ecb5791431
hshin09 <56524840+hshin09@users.noreply.github.com>
2020-12-06 09:03:17 +0700

    Update hiddenviewurl.js

--- hiddenviewurl.js
+++ hiddenviewurl.js
@@ -1,10 +1,10 @@
-function addFrame(objId) {
-	var iFrm = document.createElement('iframe');
-	iFrm.setAttribute('id', objId);
-	iFrm.setAttribute('width', '100%');
-	iFrm.setAttribute('height', '100%');
-	document.body.appendChild(iFrm);
+function addFrame(objId)
+{
+   var iFrm = document.createElement('iframe');
+   iFrm.setAttribute('id', objId);
+   iFrm.setAttribute('width', '100%');
+   iFrm.setAttribute('height', '100%');
+   document.body.appendChild(iFrm);
 }
 addFrame('TV');
-document.getElementById('TV').src
-='https://www.adintrend.tv/hd/live/i.php?ch=3&cxid=sh';
+document.getElementById('TV').src='https://www.adintrend.tv/hd/live/i.php?ch=3&cxid=sh';
