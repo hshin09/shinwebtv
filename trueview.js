e7e975bb066c977ea6add6b01bf4deb13285c407
hshin09 <56524840+hshin09@users.noreply.github.com>
2020-12-10 11:04:39 +0700

    update trueview.js

--- trueview.js
+++ trueview.js
@@ -254,8 +254,7 @@ function onright() {
 }
 
 function change() {
-	//window.trueView.showMsg("webView:if(touchscreen) oi=-1; x[ei].click()");
-   window.trueView.showMsg("webView:gi=1; change()");
+	window.trueView.showMsg("webView:if(touchscreen) oi=-1; x[ei].click()");
    window.trueView.showMsg("hideTrueView");
    tv.pause();
    web.src = '';
