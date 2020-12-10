f15c2ba26fdcb6b6886bce304b511b825643a0ea
hshin09 <56524840+hshin09@users.noreply.github.com>
2020-12-10 10:36:16 +0700

    update main1-2.js

--- main1-2.js
+++ main1-2.js
@@ -91,7 +91,7 @@ function keychk(e) {
          window.parentView.showMsg("trueView:loadMode = 0");
          //window.parentView.showMsg("trueView:full = 1; onFullscreenOnOff()");
          window.parentView.showMsg("showTrueView");
-         window.parentView.showMsg("trueView:x[si].click()");
+         window.parentView.showMsg("trueView:x[ei].click()");
       } else {
          if( svideo == 0 ) {
             svideo = 1;
