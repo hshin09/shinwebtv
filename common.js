d5ebc8bf5ededb75232fc32c9c0162686e57f86c
hshin09 <56524840+hshin09@users.noreply.github.com>
2020-12-14 09:54:15 +0700

    update common.js

--- common.js
+++ common.js
@@ -32,7 +32,7 @@ var touchscreen = 0;
 //var trueHostUrl = "https://btm007.com/stream";
 //var trueLoadUrl = trueHostUrl + "/a";
 //var path = trueHostUrl + "/pc_view.php?ch=live";
-var trueHostUrl = "https://youtv24.net";
+var trueHostUrl = "http://www.youtv24.net";
 var trueLoadUrl = trueHostUrl + "/sites";
 var path = trueLoadUrl + "/mstvs/pages/pc/pc_view.php?ch=live";
 var ch = ['26', '37', '04', '05', '25', '03', '09', '35', '30', '17', '33', '02', '01', '34', '32', '23', '14', '07', '15', '13', '50', '46', '49', '38'];
