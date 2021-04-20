0a53eb1dc0f1ea4865bd649113717c0c98b308fd
hshin09 <56524840+hshin09@users.noreply.github.com>
2021-04-20 07:09:53 +0700

    Update common.js

--- common.js
+++ common.js
@@ -107,7 +107,7 @@ function Init() {
       addr[i][3] = '79';
       tvaddr[i] = addr[i][3];
    }
-   mustWait = 1;
+   mustWait = 2;
    timer = setInterval(function() {
       OnOff();
    }, 500);
