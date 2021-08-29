32d1c4b6ec91b409719c73d05d32e6bb5a7bd739
hshin09 <56524840+hshin09@users.noreply.github.com>
2021-08-29 07:38:32 +0700

    Update common.js

--- common.js
+++ common.js
@@ -103,7 +103,7 @@ function Init() {
    stv = $('#tv').get(0);
    web = document.getElementById("web");
    for(var i = 0; i < tvaddr.length; i++) {
-      //backtvaddr[i] = '79';
+      backtvaddr[i] = '79';
       //addr[i][3] = '79';
       tvaddr[i] = addr[i][3];
    }
