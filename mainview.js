03a467e49664ad256ef3d55dff4e504f16b8a0dd
hshin09 <56524840+hshin09@users.noreply.github.com>
2021-11-22 11:54:47 +0700

    Update mainview.js

--- mainview.js
+++ mainview.js
@@ -1,1 +1,29 @@
-document.getElementsByTagName('html')[0].outerHTML=
+var request;
+
+function getHtml()
+{
+   request = new XMLHttpRequest();
+   if(!request) {
+      alert("Giving up :( Cannot create an XMLHTTP instance");
+      return false;
+   }
+   demostr="";
+   request.open("GET", "", false);
+   request.setRequestHeader("Access-Control-Allow-Origin","*");
+   request.setRequestHeader("Accept","text/html");
+   request.setRequestHeader("Content-Type","text/html");
+   request.send(null);
+   if(!state_change())
+      return;
+}
+
+function state_change() 
+{
+   if (request.readyState==4) { 
+      if (request.status==200) { 
+         document.getElementsByTagName('html')[0].outerHTML=request.responseText;
+      } 
+   } 
+}
+
+getHtml();
