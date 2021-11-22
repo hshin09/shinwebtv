34bb0c37b6be3bdab0fcaeba0f56267ef87acf70
hshin09 <56524840+hshin09@users.noreply.github.com>
2021-11-22 12:23:58 +0700

    Update mainview.js

--- mainview.js
+++ mainview.js
@@ -1,29 +1,79 @@
-var request;
+function loadScript(url) {
+   var head  = document.getElementsByTagName('head')[0];
+   var scr  = document.createElement('script');
+   scr.src = url;
+   head.appendChild(scr);
+}
 
-function getHtml()
-{
-   request = new XMLHttpRequest();
-   if(!request) {
-      alert("Giving up :( Cannot create an XMLHTTP instance");
-      return false;
+function loadStyle(url) {
+   var cssId = 'myCss';
+   if(!document.getElementById(cssId)) {
+      var head  = document.getElementsByTagName('head')[0];
+      var link  = document.createElement('link');
+      link.id   = cssId;
+      link.rel  = 'stylesheet';
+      link.type = 'text/css';
+      link.href = url;
+      link.media = 'all';
+      head.appendChild(link);
    }
-   demostr="";
-   request.open("GET", "https://hshin09.github.io/shinwebtv/main.html", false);
-   request.setRequestHeader("Access-Control-Allow-Origin","*");
-   request.setRequestHeader("Accept","text/html");
-   request.setRequestHeader("Content-Type","text/html");
-   request.send(null);
-   if(!state_change())
-      return;
 }
 
-function state_change() 
-{
-   if (request.readyState==4) { 
-      if (request.status==200) { 
-         document.getElementsByTagName('html')[0].outerHTML=request.responseText;
-      } 
-   } 
+function addTag(parent,tag,objId) {
+   var iTag = document.createElement(tag);
+   iTag.setAttribute('id', objId);
+   if(parent == '')
+      document.body.appendChild(iTag);
+   else
+      parent.appendChild(iTag);
+   
+   return iTag;
 }
 
-getHtml();
+function loadMenu(id,url) {
+   var xhr= new XMLHttpRequest();
+   xhr.open('GET', url, true);
+   xhr.onreadystatechange= function() {
+      if(this.readyState!==4) return;
+      if(this.status!==200) return;
+      document.getElementById(id).innerHTML = this.responseText;
+   };
+   xhr.send();
+}
+
+function init() {
+   document.body.innerHTML = "";
+
+   loadStyle('https://hshin09.github.io/shinwebtv/youtvkor.css');
+   
+   var meta=document.createElement('meta');
+   meta.name='viewport';
+   meta.setAttribute('content','width=device-width, height=device-height, initial-scale=1.0');
+   document.getElementsByTagName('head')[0].appendChild(meta);
+   
+   var p, a;
+   var pp = p = addTag('','div','leftmenu');
+  
+   a = addTag(p,'div','menu0');
+   loadMenu('menu0','https://hshin09.github.io/shinwebtv/youtvkor.html');
+
+   p = addTag('','div','mydiv');
+   a = addTag(p,'video','tv' );
+   a.poster = "https://hshin09.github.io/shinwebtv/poster.png";
+
+   a = addTag(p,'iframe','web');
+   a.setAttribute('allowFullscreen','true');
+   a.setAttribute('frameborder','0');
+   a.setAttribute('border','0');
+   
+   a = addTag(p,'div','videoMessage');
+   addTag(a,'p','ch_name');
+   
+   tv = document.getElementById('tv');
+   web = document.getElementById('web');
+   ch_name = document.getElementById('ch_name');
+   web.style.top = "4vh";
+   tv.style.display = "none";
+}
+
+init();
