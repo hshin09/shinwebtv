3c8e5dae1c6c7dff871c10902d9d2c6495315136
hshin09 <56524840+hshin09@users.noreply.github.com>
2021-11-24 10:21:03 +0700

    Create tvchak.js

---
+++ tvchak.js
@@ -0,0 +1,65 @@
+function loadScript(url) {
+   var head  = document.getElementsByTagName('head')[0];
+   var scr  = document.createElement('script');
+   scr.src = url;
+   head.appendChild(scr);
+}
+
+function loadStyle(url) {
+   var cssId = 'myCss';
+   if(!document.getElementById(cssId)) {
+      var head  = document.getElementsByTagName('head')[0];
+      var link  = document.createElement('link');
+      link.id   = cssId;
+      link.rel  = 'stylesheet';
+      link.type = 'text/css';
+      link.href = url;
+      link.media = 'all';
+      head.appendChild(link);
+   }
+}
+
+function addTag(parent,tag,objId) {
+   var iTag = document.createElement(tag);
+   iTag.setAttribute('id', objId);
+   if(parent == '')
+      document.body.appendChild(iTag);
+   else
+      parent.appendChild(iTag);
+   
+   return iTag;
+}
+
+function loadMenu(id,url) {
+   var xhr= new XMLHttpRequest();
+   xhr.open('GET', url, true);
+   xhr.onreadystatechange= function() {
+      if(this.readyState!==4) return;
+      if(this.status!==200) return;
+      document.getElementById(id).innerHTML = this.responseText;
+   };
+   xhr.send();
+}
+
+function init() {
+   document.body.innerHTML = "";
+
+   var p = addTag('','div','main');
+   loadMenu('p','https://hshin09.github.io/shinwebtv/main.html');
+}
+ 
+function getHtml(url)
+{ 
+  var xhr= new XMLHttpRequest();
+   xhr.open('GET', url, true);
+   xhr.onreadystatechange= function() {
+      if(this.readyState!==4) return;
+      if(this.status!==200) return;
+      alert(this.responseText);
+      document.getElementsByTagName('html')[0].innerHTML = this.responseText;
+   };
+   xhr.send();
+} 
+
+//getHtml('https://hshin09.github.io/shinwebtv/main.html');
+init();
