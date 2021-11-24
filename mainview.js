fbdf38fc2d605fd7efa4a7df4323af342d958d5b
hshin09 <56524840+hshin09@users.noreply.github.com>
2021-11-24 17:47:42 +0700

    Update mainview.js

--- mainview.js
+++ mainview.js
@@ -9,9 +9,9 @@ function init()
 
    document.querySelector('head').id="head";
    document.querySelector('body').id="body"; 
+   $('#head').load("https://hshin09.github.io/shinwebtv/main.html #mainhead");
 alert(document.querySelector('html').outerHTML);
 
-   $('#head').load("https://hshin09.github.io/shinwebtv/main.html #mainhead");
    $('#body').load("https://hshin09.github.io/shinwebtv/main.html #mainbody");
 
    ss = document.createElement('script');
