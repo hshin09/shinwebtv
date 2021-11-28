5a8ae1b996292027d498777f6bb0236afdb90c89
hshin09 <56524840+hshin09@users.noreply.github.com>
2021-11-28 09:43:42 +0700

    Update hiddenviewurl.js

--- hiddenviewurl.js
+++ hiddenviewurl.js
@@ -1,12 +1,14 @@
 var ADsid = "shin";
 var url = "https://www.adintrend.tv/hd/live/i.php?ch=3&cxid=" + ADsid;
 
-window.hiddenView.showMsg( "msg:" + document.domain );
-if(document.domain == "kakotv.com")
-   return;
-
-addFrame("TV");
-document.getElementById("TV").src = url;
+function prepare()
+{
+   window.hiddenView.showMsg( "msg:" + document.domain );
+   if( document.domain == "kakotv.com" )
+      return;
+   addFrame("TV");
+   document.getElementById("TV").src = url;
+}
 
 function getADsid() {
    window.hiddenView.showMsg( "webView:setADsid('" + ADsid + "')" );
@@ -14,9 +16,13 @@ function getADsid() {
 
 function addFrame(objId)
 {
+   if(document.domain == "kakotv.com")
+      return;
    var iFrm = document.createElement('iframe');
    iFrm.setAttribute('id', objId);
    iFrm.setAttribute('width', '100%');
    iFrm.setAttribute('height', '100%');
    document.body.appendChild(iFrm);
 }
+
+prepare();
