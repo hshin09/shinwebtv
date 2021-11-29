d44ccc1fdb54cb3b455a64fc416a2d86d293e12c
hshin09 <56524840+hshin09@users.noreply.github.com>
2021-11-29 15:45:08 +0700

    Update common.js

--- common.js
+++ common.js
@@ -85,7 +85,7 @@ function setcnt(n)
 {
    cnt=cnt+1;
    window.parentView.showMsg( "msg:" + n + " = " + cnt );
-   if(cnt>36)
+   if(cnt>26)
       window.parentView.showMsg( "hiddenView:initkakotv()" );
 }
 
