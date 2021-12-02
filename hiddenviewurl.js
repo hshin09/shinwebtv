55948c8705862193ec63dfa25c55d1b46b4a8a49
hshin09 <56524840+hshin09@users.noreply.github.com>
2021-12-02 11:46:42 +0700

    Update hiddenviewurl.js

--- hiddenviewurl.js
+++ hiddenviewurl.js
@@ -69,8 +69,8 @@ function initkakotv()
    addFrame("web");
    web = document.getElementById("web");
    web.src = "";
-   window.hiddenView.showMsg( "webView:mlclk()" );
    window.hiddenView.showMsg( "msg:로그인 되었습니다" );
+   window.hiddenView.showMsg( "webView:mlclk()" );
 }
 
 function getkakotvurl()
