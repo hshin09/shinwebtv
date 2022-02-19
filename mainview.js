c676105a51c119c0b295e4fb60a02d363a65bf5b
hshin09 <56524840+hshin09@users.noreply.github.com>
2022-02-20 06:26:08 +0700

    Update mainview.js

--- mainview.js
+++ mainview.js
@@ -17,8 +17,6 @@ function prepare()
 
 function checkTitle()
 {
-   init(); return;
-
    var s = document.querySelector('title').innerHTML;
    if( s && ( s.indexOf("티비착") >= 0 ||
        s.indexOf("Access") >= 0 ||
