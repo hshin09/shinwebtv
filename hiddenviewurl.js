0e9b86aac47c1422443e58685f706a19b471c7d4
hshin09 <56524840+hshin09@users.noreply.github.com>
2022-10-24 08:28:59 +0700

    Update hiddenviewurl.js

--- hiddenviewurl.js
+++ hiddenviewurl.js
@@ -8,15 +8,15 @@ var url = "https://www.adintrend.tv/hd/live/i.php?ch=3&cxid=" + ADsid;
 
 function prepare()
 {
-   window.hiddenView.showMsg( "msg:" + location.href );   
+   //window.hiddenView.showMsg( "msg:" + location.href );   
    if( location.href == "https://kakotv.com/" ) {
-      window.hiddenView.showMsg( "msg1:" + location.href );
+      window.hiddenView.showMsg( "msg:1-" + location.href );
       callLogin('hsh09', 'sh0903');
       setTimeout(function(){initkakotv();},1000);
       return;
    }
    if( location.href.indexOf("kakotv.com") > 0 ) {
-      window.hiddenView.showMsg( "msg2:" + location.href );
+      window.hiddenView.showMsg( "msg:2-" + location.href );
       initkakotv();
       return;
    }
