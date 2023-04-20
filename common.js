f141e1e557a5929480d66bff00d3feb2e7c449a5
hshin09 <56524840+hshin09@users.noreply.github.com>
2023-04-20 16:04:58 +0700

    Update common.js

--- common.js
+++ common.js
@@ -15,14 +15,14 @@ var addr = [
   ["Cable Movies", "중화 TV", "영화 Screen", "79", "/%EC%A4%91%ED%99%94%20TV-101", "79", 3],
   ["CBSN", "ABC News", "CNA", 
      "https://cbsn-us.cbsnstream.cbsnews.com/out/v1/55a8648e8f134e82a470f83d562deeca/master.m3u8",
-     " https://content.uplynk.com/channel/3324f2467c414329b3b0cc5cd987b6be.m3u8",
+     "https://content.uplynk.com/channel/3324f2467c414329b3b0cc5cd987b6be.m3u8",
      "https://d2e1asnsl7br7b.cloudfront.net/7782e205e72f43aeb4a48ec97f66ebbe/index.m3u8", 3],
   ["Music Top", "Hits 360", "none", 
      "https://live-edge01.telecentro.net.ar/live/smil:musictop.smil/playlist.m3u8",
-     "http://cm.hostlagarto.com:8081/hits360tv/hits360HD.myStream/playlist.m3u8","none", 3],
+     "web/http://cm.hostlagarto.com:8081/hits360tv/hits360HD.myStream/playlist.m3u8","none", 3],
   ["FasionTV", "Auto Motor", "none", 
      "https://lb.streaming.sk/fashiontv/stream/playlist.m3u8",
-     "http://100automoto.tv:1935/bgtv1/Stream1/playlist.m3u8","none", 3],
+     "web/http://100automoto.tv:1935/bgtv1/Stream1/playlist.m3u8","none", 3],
   ["NHK WORLD", "WINI Live", "NTV News", 
      "https://nhkwlive-xjp.webcdn.stream.ne.jp/hls/live/2003458/nhkwlive-xjp-en/index_1M.m3u8", 
      "https://movie.mcas.jp/mcas/wn1_2/master.m3u8", 
@@ -613,8 +613,8 @@ function movieclk(w, url, p)
 
    if(url.substr(0,4) == "web/") {
       isHotKey = 0;
-      imsi_tvchak = 1;
-      tvchak = 1;
+      imsi_tvchak = 0;
+      tvchak = 0;
       url = url.substr(4);
       w = "web";
    }
