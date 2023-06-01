3694c96e614d981ce21519682f945b59064fccdc
hshin09 <56524840+hshin09@users.noreply.github.com>
2023-06-01 11:54:27 +0700

    Update main1-2.js

--- main1-2.js
+++ main1-2.js
@@ -172,6 +172,7 @@ function keychk(e) {
    else if( e.which == 48 ) { //0
       if( gi == 0 ) {
 	 window.parentView.showMsg("hiddenView:directLoadVideo = !directLoadVideo");
+	 window.parentView.showMsg("msg:directLoadVideo = " + directLoadVideo);
 	 /*
          changeKorTv();
          setTimeout(function(){ mlok(); }, 500);
